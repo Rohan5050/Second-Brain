62bd5bc1311606cb48640a6d791999fe32a41f36
Rohan Vohra <rohanvohra055@gmail.com>
2025-01-02 17:09:01 +0530

    update index.ts

--- Brainly-backend/src/index.ts
+++ Brainly-backend/src/index.ts
@@ -7,7 +7,9 @@ import cors from "cors";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
 
 // Sign-up route
 app.post("/api/v1/signup", async (req, res) => {
