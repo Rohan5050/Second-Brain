748d2b62a1ddcac4f0189320fe575b03600591ac
Rohan Vohra <rohanvohra055@gmail.com>
2025-01-02 17:42:40 +0530

    updated package.json

--- Brainly-Frontend/react-app/src/hooks/useContent.tsx
+++ Brainly-Frontend/react-app/src/hooks/useContent.tsx
@@ -8,17 +8,17 @@ export function useContent () {
     const [contents, setContents] = useState([]);
 
     function refresh () {
-        axios.get(`${BACKEND_URL}/api/v1/content`, {
+        return axios.get(`${BACKEND_URL}/api/v1/content`, {
             headers: {
                 "Authorization": localStorage.getItem("token")
             }
         })
-         
-        
-        
         .then((response) => {
             setContents(response.data.content)
         })
+        .catch((error) => {
+            console.error("Error fetching content:", error);
+        });
     }
 
     useEffect(() => {
